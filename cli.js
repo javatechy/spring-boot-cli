8e3f1feb77cc062ace4ccc7f67ee2facff5fbb28
deepak.kumar <deepak@deepak.local>
2018-01-22 12:56:05 +0530

    added sample json

--- cli.js
+++ cli.js
@@ -2,21 +2,28 @@
 var fs = require('fs');
 var path = require('path');
 var readline = require('readline');
+
+var request = require('./templateConfig.json');
 var readInput = readline.createInterface(process.stdin, process.stdout);
 
 // Location of the script
 var scriptLoc = process.argv[1];
+console.log("Current File running => "+scriptLoc);
+console.log("Current File templateJson => "+request.temp);
 // reading file utils
 var fileUtils = require('./fileUtils.js');
 // Operation to perform
 var operationArg = process.argv[2];
+var content = fileUtils.replaceContent("${hel}","hesl","peace");
 
 // git clone https://github.com/javatechy/spring-boot-template.git .
 fileUtils.createDir("/Users/deepak/Desktop/temp");
 fileUtils.createFile("/Users/deepak/Desktop/temp/abc.txt");
 fileUtils.writeDataInFile("/Users/deepak/Desktop/temp/abc.txt","writeDataInFile");
-var content = fileUtils.replaceContent("${hel}","hesl","peace");
 
+console.log("Current Path => "+ __dirname);
+
+var content = fileUtils.replaceContent("${hel}","hel","peace");
 console.log(process.argv[2]+"content=>"+content);
 
 switch (operationArg) {
